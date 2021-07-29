62f1328ae7cad2494b77487406aa0cb04e1a1408
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-29 10:53:41 -0500

    vale verga la vida

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider, connect } from 'react-redux';
 //import thunk from 'redux-thunk';
-var ReduxThunk = require('redux-thunk').default
+var ReduxThunk = require('redux-thunk')
 //import logger from 'redux-logger'
 
 
@@ -47,7 +47,7 @@ const reducer = (state=initialState, action) => {
 	}
 }
 
-const store = createStore(reducer, applyMiddleware(ReduxThunk));
+const store = createStore(reducer, applyMiddleware(ReduxThunk.default));
 
 //store.dispatch({ type: FETCH_DATA })
 /*fetch('http://quotes.stormconsultancy.co.uk/random.json')
